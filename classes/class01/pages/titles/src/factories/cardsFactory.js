7c1475ae9a44b0f47687285d176c10c965892900
Ana Paula Rossi Menezes <anapaularossi.577@gmail.com>
2023-03-01 21:53:14 -0300

    finished first class

--- classes/class01/pages/titles/src/factories/cardsFactory.js
+++ classes/class01/pages/titles/src/factories/cardsFactory.js
@@ -3,17 +3,15 @@ import CardsView from "./../views/cardsView.js"
 import CardsService from "./../services/cardsService.js"
 
 const cardListWorker = new Worker(`./src/workers/cardListWorkers.js`, { type: "module" })
-cardListWorker.onmessage = (msg) => {
-  console.log('processo pricipal', msg)
-}
-cardListWorker.postMessage('hey, hello!!')
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initalize() {
     return CardsController.initialize({
-      worker: cardListWorker,
       view: new CardsView(),
-      service: new CardsService({ dbUrl: `${rootPath}/assets/database.json` })
+      service: new CardsService({
+        dbUrl: `${rootPath}/assets/database.json`,
+        cardListWorker
+      })
     })
   }
 }
